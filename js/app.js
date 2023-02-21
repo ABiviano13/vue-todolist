64b707c601bf0c3e64777736232647cd2dd00d9e
ABiviano13 <abiviano0@outlook.com>
2023-02-21 15:51:26 +0100

    FEAT Milestone 1

--- js/app.js
+++ js/app.js
@@ -1,9 +1,33 @@
 const { createApp } = Vue
 
+const arrayToDo = [
+    {
+        text: 'Fare la spesa',
+        done: true
+    },
+    {
+        text: 'Completare progetto ToDoList',
+        done: true
+    },
+    {
+        text: 'Controllare le mail',
+        done: false
+    },
+    {
+        text: 'Andare a correre',
+        done: true
+    },
+    {
+        text: 'Fare regalo a Mario',
+        done: false
+    },
+
+]
+
 createApp({
     data() {
       return {
-        message: 'Hello Vue!'
+        arrayToDoList: arrayToDo,
       }
     }
 }).mount('#app')
